2b39e35e4c08d9d524a3863ebd51d79bdabb24ab
Konstantin Kraska <konstantin.kraska@hs-augsburg.de>
2019-05-22 00:49:00 +0200

    added style and css-loader, cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,27 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/index.js',
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };

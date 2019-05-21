36891f95c5b2d9ded5f2a30f4bdf2fec1a3e9c38
Konstantin Kraska <konstantin.kraska@hs-augsburg.de>
2019-05-21 23:21:25 +0200

    added code conventions

---
+++ js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+/*
+* Attention follow these rules:
+* 1. Use strict mode: "use strict"
+* 2. Use single quotes -> eslint set
+* 3. Use function expression -> eslint set
+* 4. Name variables in camelCase -> eslint set
+* 5. Clean commenting ->
+* 6. Clear structure (new lines / group code) -> eslint set
+* 7. Cleanup before production
+*/
+
+const assHole = 'hey';
+
+const test = (p) => {console.log(p);};
+
+test(assHole);

c6db8685cdd77e51499e3cac907f059d0882b003
kraskasystems <office@kraska-systems.de>
2019-07-14 23:31:45 +0200

    added jsdoc comments

--- src/js/classes/Brush.js
+++ src/js/classes/Brush.js
@@ -1,9 +1,22 @@
+/**
+ * @class Brush
+ * instantinates a simple brush
+ */
+
 class Brush {
+  /**
+   * @constructor
+   * @param {HTMLElement} pElement - html element acting as control to activate the tool
+   */
   constructor(pElement){
     this.element = pElement;
     this.size = 3;
   }
 
+  /**
+   * method used to change the size of the brush
+   * @param {event} event - wheel event that triggered method call
+   */
   changeBrushSize(event) {
 
     if(event.deltaY < 0 && this.size < 200){
